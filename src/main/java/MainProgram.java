67d793bb5915bce74d98dcd45c44afad250087c9
Matt-NBR <66144847+Matt-NBR@users.noreply.github.com>
2020-08-02 18:39:48 -0500

    Add files via upload

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,9 +1,59 @@
 
+import java.util.Arrays;
+
+
 public class MainProgram {
 
     public static void main(String[] args) {
         // write your test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+        sort(array);
+    }
+    
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+        return smallest;
+    }
+    
+    public static int indexOfSmallest(int[] array) {
+        int smallest = smallest(array);
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == smallest) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    
+    public static int indexOfSmallestFrom(int[] table, int startIndex) {
+        int smallest = table[startIndex];
+        int index = startIndex;
+        for (int i = startIndex; i < table.length; i++) {
+            if (table[i] < smallest) {
+                smallest = table[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    
+    public static void swap(int[] array, int index1, int index2) {
+        int aux = array[index1];
+        array[index1] = array[index2];
+        array[index2] = aux;
+    }
+    public static void sort(int[] array) {
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            swap(array, i, indexOfSmallestFrom(array, i));
+            System.out.println(Arrays.toString(array));
+        }
     }
 
 }
